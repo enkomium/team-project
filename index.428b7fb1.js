c8042e484646addda15ce988baf9ee35cb890bfc
enkomium <96664472+enkomium@users.noreply.github.com>
2023-01-27 12:37:34 +0000

    Deploying to gh-pages from @ enkomium/team-project@d9071a87d082d42cfc84984685309a312fe48f6a 🚀

---
+++ index.428b7fb1.js
@@ -0,0 +1,2 @@
+(()=>{const o={openModalBtn:document.querySelector("[data-modal-thanks-open]"),closeModalBtn:document.querySelector("[data-modal-thanks-close]"),modal:document.querySelector("[data-modal-thanks]")};function e(){document.body.classList.toggle("modal-thanks-open"),o.modal.classList.toggle("backdrop--is-hidden")}o.openModalBtn.addEventListener("click",e),o.closeModalBtn.addEventListener("click",e)})();
+//# sourceMappingURL=index.428b7fb1.js.map
